bd0c695b381296af375c2e18c9dd64e130427e9f
Akash Gajjar <akashgajjar8@gmail.com>
2019-04-30 17:38:21 +0530

    deploy site on gh-pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380adf610922866d450d1a7155a60b54.js"
+  "/precache-manifest.ec93e184ec81b25471824c3fc6058d82.js"
 );
 
 self.addEventListener('message', (event) => {
