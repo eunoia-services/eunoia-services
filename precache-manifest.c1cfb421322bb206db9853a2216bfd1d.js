2cc918353fc1a762f355c024fb4a1cf7274bf6c7
Akash Gajjar <akashgajjar8@gmail.com>
2019-05-02 16:56:17 +0530

    optimize images in producition build

--- precache-manifest.c1cfb421322bb206db9853a2216bfd1d.js
+++ precache-manifest.c1cfb421322bb206db9853a2216bfd1d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5442b29b91a50822848e262d712cadfc",
+    "revision": "798ac39c1aeb80a83ef882cac1a6702d",
     "url": "/eunoia-services/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/eunoia-services/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "bf89a0824af78873e987",
-    "url": "/eunoia-services/static/css/main.de647ac2.chunk.css"
+    "revision": "800eb69d819b7f8131f6",
+    "url": "/eunoia-services/static/css/main.ba9e76a8.chunk.css"
   },
   {
     "revision": "6c21c51cd58bd3be645d",
     "url": "/eunoia-services/static/js/2.155b87a8.chunk.js"
   },
   {
-    "revision": "bf89a0824af78873e987",
+    "revision": "800eb69d819b7f8131f6",
     "url": "/eunoia-services/static/js/main.c888f96a.chunk.js"
   },
   {
@@ -28,36 +28,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/eunoia-services/static/media/calendar.07b46a3d.svg"
   },
   {
-    "revision": "8a35298d5254ab5c09ed2c01c447b2ba",
-    "url": "/eunoia-services/static/media/carpet_clean.8a35298d.webp"
+    "revision": "f6f0d6c8c667a3afb5d459cc8046b407",
+    "url": "/eunoia-services/static/media/carpet_clean.f6f0d6c8.jpg"
   },
   {
     "revision": "4515f8af89edb4af2b97c04c51f9f73a",
     "url": "/eunoia-services/static/media/circle-ok.4515f8af.svg"
   },
   {
-    "revision": "bbe1efeeafc8563bc1270b3531430aaf",
-    "url": "/eunoia-services/static/media/cleaner.bbe1efee.jpg"
-  },
-  {
-    "revision": "76351c0fdf8499120408a9c9cee25399",
-    "url": "/eunoia-services/static/media/commercial_clean.76351c0f.webp"
+    "revision": "965bba910cb9010fe4359fb0801c0c38",
+    "url": "/eunoia-services/static/media/cleaner.965bba91.jpg"
   },
   {
     "revision": "acf86b3c346541bc31f985dd0af77942",
     "url": "/eunoia-services/static/media/happy-face.acf86b3c.svg"
-  },
-  {
-    "revision": "d2ba1a9de31e187c511f5f417d21453f",
-    "url": "/eunoia-services/static/media/home_clean.d2ba1a9d.webp"
-  },
-  {
-    "revision": "6168ca494f66ccbf0dbc85635a5bd847",
-    "url": "/eunoia-services/static/media/maid_service.6168ca49.webp"
-  },
-  {
-    "revision": "cfe2e413792219cddcb85086ee378889",
-    "url": "/eunoia-services/static/media/move_in_clean.cfe2e413.webp"
   },
   {
     "revision": "06cc25332cb4023e18540f95641d6302",
@@ -66,9 +50,5 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "7de13d6ceb8f3d72a27ce96a6a9bea18",
     "url": "/eunoia-services/static/media/up.7de13d6c.svg"
-  },
-  {
-    "revision": "d6297d75921d0ba0a43b5635e6720216",
-    "url": "/eunoia-services/static/media/window_clean.d6297d75.webp"
   }
 ]);
